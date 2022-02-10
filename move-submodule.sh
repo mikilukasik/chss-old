6327094863013e482f8a8021a11c95fcb4a93b3f
mikilukasik <szarhazi@gmail.com>
2022-02-10 19:02:11 +0000

    add service-task-manager

--- move-submodule.sh
+++ move-submodule.sh
@@ -1,56 +1,41 @@
-# staringfolder="${PWD}"
 sharedproject="${PWD}/msg"
-# superproject="${PWD}/chss-app-game-client"
-# submodule="msg"
-# cd "$superproject"
-# (cd -- "$submodule" && git status) # Verify that no uncommited changes exist!
-# (cd -- "$submodule" && git push -- "$sharedproject") # Save any local-only commits
-# git submodule deinit -f -- "$submodule" # Get rid of submodule's check-out
-# rm -rf .git/modules/"$submodule" # as well as of its local repository
-# mkdir -p .submods
-# git mv -- "$submodule" .submods/
-# echo "gitdir: $sharedproject.git" > ".submods/$submodule/.git"
-# ln -s -- "$sharedproject" "$submodule"
-# echo "/$submodule" >> .gitignore
-# cd "$staringfolder"
 
-project="${PWD}/chss-app-game-client"
-rm -rf "${project}/msg"
-ln -s -- "$sharedproject" "$project"
+  project="${PWD}/chss-service-task-manager"
+  rm -rf "${project}/msg"
+  ln -s -- "$sharedproject" "$project"
 
-project="${PWD}/chss-service-auth"
-rm -rf "${project}/msg"
-ln -s -- "$sharedproject" "$project"
+  project="${PWD}/chss-app-game-client"
+  rm -rf "${project}/msg"
+  ln -s -- "$sharedproject" "$project"
 
+  project="${PWD}/chss-service-auth"
+  rm -rf "${project}/msg"
+  ln -s -- "$sharedproject" "$project"
 
-project="${PWD}/chss-service-game-handler"
-rm -rf "${project}/msg"
-ln -s -- "$sharedproject" "$project"
+  project="${PWD}/chss-service-game-handler"
+  rm -rf "${project}/msg"
+  ln -s -- "$sharedproject" "$project"
 
+  project="${PWD}/chss-service-gateway"
+  rm -rf "${project}/msg"
+  ln -s -- "$sharedproject" "$project"
 
-project="${PWD}/chss-service-gateway"
-rm -rf "${project}/msg"
-ln -s -- "$sharedproject" "$project"
-
-
-project="${PWD}/chss-service-logger"
-rm -rf "${project}/msg"
-ln -s -- "$sharedproject" "$project"
+  project="${PWD}/chss-service-logger"
+  rm -rf "${project}/msg"
+  ln -s -- "$sharedproject" "$project"
 
 sharedproject="${PWD}/chss-module-engine"
 
-project="${PWD}/chss-app-game-client"
-rm -rf "${project}/chss-module-engine"
-ln -s -- "$sharedproject" "$project"
+  project="${PWD}/chss-app-game-client"
+  rm -rf "${project}/chss-module-engine"
+  ln -s -- "$sharedproject" "$project"
 
-project="${PWD}/chss-service-game-handler"
-rm -rf "${project}/chss-module-engine"
-ln -s -- "$sharedproject" "$project"
+  project="${PWD}/chss-service-game-handler"
+  rm -rf "${project}/chss-module-engine"
+  ln -s -- "$sharedproject" "$project"
 
 sharedproject="${PWD}/chss-module-logger"
 
-project="${PWD}/chss-app-game-client"
-rm -rf "${project}/chss-module-logger"
-ln -s -- "$sharedproject" "$project"
-
-
+  project="${PWD}/chss-app-game-client"
+  rm -rf "${project}/chss-module-logger"
+  ln -s -- "$sharedproject" "$project"
